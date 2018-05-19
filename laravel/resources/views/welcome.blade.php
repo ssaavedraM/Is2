123365531c2e8cd3f5fdd821b53bfef3ddc9ed03
Sergio Saavedra Muñoz <sesaavedra@udec.cl>
2018-05-19 01:21:09 -0400

    aun no se usar esto :c

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
 
         <!-- Styles -->
         <style>
@@ -66,24 +67,22 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+             <img class="w3-circle w3-btn w3-display-topleft w3-padding-16" alt="Norway" src="acelera.jpg" onclick="window.location.href='http://www.aceleralatam.cl'" style="max-width: 10%">
+             
+             <div class="top-right links">
+                    <a href="">Ingreso</a>
+                    <a href="">Registro</a>
                 </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
                     Formulario Prueba IS2
                     Es horrible D:
+
                 </div>
 
                 <div class="links">
+                    <a href="http://www.aceleralatam.cl/inicio/">AceleraLatam</a>
                     <a href="https://laravel.com/docs">Documentacion</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">Nuevos</a>
